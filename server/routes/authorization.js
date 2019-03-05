01b0ff7a02fbce78b17603a97fe627030b835ee5
murediane <murediana@gmail.com>
2019-03-05 22:04:28 +0200

    create user login endpoint

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -1,10 +1,10 @@
 import Router from 'express';
 import { createUser } from '../controllers/user';
-import { createAuth } from '../controllers/auth';
+import { login } from '../controllers/auth';
 const router = Router();
 const entry = '/auth';
 // create Meetups route
 
-router.post(`${entry}/user`, createUser);
-router.post(`${entry}/signup`, createAuth);
+router.post(`${entry}/signup`, createUser);
+router.post(`${entry}/login`, login);
 export default router;
