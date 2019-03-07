33a2a65e3c349179f826f92a6595de66156336d2
murediane <murediana@gmail.com>
2019-03-07 20:11:39 +0200

    integrate coveralls

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,18 +4,24 @@ import bcrypt from 'bcrypt';
 import Jwt from 'jsonwebtoken';
 import _ from 'lodash';
 const login = async (req, res) => {
-  const { error } = validateAuth(req.body);
-
-  if (error) {
-    res.status(404).send(error.details[0].message);
-    return;
+  try {
+    await validateAuth(req.body);
+    let user = await users.find(u => u.email === req.body.email);
+    if (!user)
+      return res.json({ status: 400, message: 'invalid email or password' });
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword)
+      return res
+        .status(400)
+        .json({ status: 400, message: 'invalid email or password' });
+    const token = Jwt.sign({ _email: user._email }, 'jwtPrivateKey');
+    return res.status(201).json({ status: 201, token });
+  } catch (error) {
+    if (error.details) {
+      res.status(400).json({ message: error.details[0].message });
+      return;
+    }
+    return res.status(500).json({ message: 'Unexpected error happened.' });
   }
-  let user = await users.find(u => u.email === req.body.email);
-  if (!user) return res.send({ status: 400, message: 'invalid email or password' });
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword)
-    return res.send({ status: 400, message: 'invalid email or password' });
-  const token = Jwt.sign({ _email: user._email }, 'jwtPrivateKey');
-  res.send({ status: 201, token });
 };
 export { login };
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,25 +2,33 @@ import { validateUser } from '../helpers/validator';
 import { users } from '../models';
 import bcrypt from 'bcrypt';
 import Jwt from 'jsonwebtoken';
-import config from 'config';
 const createUser = async (req, res) => {
-  const { error } = validateUser(req.body);
-
-  if (error) {
-    res.status(404).send(error.details[0].message);
-    return;
+  try {
+    await validateUser(req.body);
+    let user = await users.find(u => u.email === req.body.email);
+    if (user)
+      return res.status(400).json({ status: 400, message: 'user already exist' });
+    user = { id: users.length + 1, ...req.body };
+    const salt = await bcrypt.genSalt(15);
+    user.password = await bcrypt.hash(user.password, salt);
+    const token = Jwt.sign(
+      {
+        _email: user._email,
+        _firstName: user._firstName,
+        _lastName: user._lastName
+      },
+      'jwtPrivateKey'
+    );
+    await users.push(user);
+    return res.status(201).json({ status: 201, token });
+  } catch (error) {
+    if (error.details) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    // handle every error thrown by the promise rejections
+    return res.status(500).json({ message: 'Unexpected error happened.' });
   }
-  let user = await users.find(u => u.email === req.body.email);
-  if (user) return res.send({ status: 400, message: 'user already exist' });
-  user = { id: users.length + 1, ...req.body };
-  const salt = await bcrypt.genSalt(15);
-  user.password = await bcrypt.hash(user.password, salt);
-  const token = Jwt.sign(
-    { _email: user._email, _firstName: user._firstName, _lastName: user._lastName },
-    'jwtPrivateKey'
-  );
-  await users.push(user);
-  return res.send({ status: 201, token });
+
   // });
 };
 export { createUser };
