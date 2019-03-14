58a39233d4efbe67af5c263983f03e6ff25669c7
murediane <murediana@gmail.com>
2019-03-14 10:38:56 +0200

    add swagger on the documentation

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import constants from './config/constants';
 import middleware from './middleware';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger';
 
 const server = express();
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use(morgan('dev'));
 server.use(express.json());
 
