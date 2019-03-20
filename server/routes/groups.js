38b9318986741871655b874e1e51fad3ca1c599a
murediane <murediana@gmail.com>
2019-03-20 11:30:36 +0200

    [164629471] delete group if you are the groupadmin

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,7 +1,7 @@
 import Router from 'express';
 import checkToken from '../middleware/checkToken';
 import { validateNewGroup } from '../middleware/validateData';
-import { createGroups, getAllGroups, updateGroup } from '../controllers/groups';
+import { createGroups, getAllGroups, updateGroup ,deleteGroup} from '../controllers/groups';
 
 const router = Router();
 const groupEntry = '/groups';
@@ -9,5 +9,6 @@ const groupEntry = '/groups';
 
 router.post(`${groupEntry}`, checkToken, validateNewGroup, createGroups);
 router.get(`${groupEntry}`, checkToken, getAllGroups);
-router.patch(`${groupEntry}/:id`, checkToken, validateNewGroup, updateGroup);
+router.patch(`${groupEntry}/:id/name`, checkToken, validateNewGroup, updateGroup);
+router.delete(`${groupEntry}/:id`, checkToken, deleteGroup);
 export default router;
