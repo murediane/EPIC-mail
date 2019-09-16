066983fa008f4bb7053a187b2bf008ebbdc24059
murediane <murediana@gmail.com>
2019-09-16 20:31:52 +0200

    Fix cors

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -3,18 +3,26 @@ import routes from '../routes';
 export default server => {
   /** ********* Headers configuration *************** */
 
+  // server.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+  //   );
+  //   res.header('Access-Control-Expose-Header', 'X-Total-Count');
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,PATCH,DELETE');
+  //     return res.status(200).json({});
+  //   }
+  //   next();
+  // });
+
   server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin,X-Requested-With,Content-Type,Accept,Authorization'
-    );
-    res.header('Access-Control-Expose-Header', 'X-Total-Count');
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,PATCH,DELETE');
-      return res.status(200).json({});
-    }
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, access-token');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
   });
 
   /** ********* RESTful APIs' endpoints ************* */
